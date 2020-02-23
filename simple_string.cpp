9dcd438192c606f6640f9242e02a2c40155642e6
PROgrammer_JARvis <gh@progrm-jarvis.ru>
2020-02-23 20:54:31 +0300

    Add basic class components

--- simple_string.cpp
+++ simple_string.cpp
@@ -1,1 +1,109 @@
 #include "simple_string.h"
+
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <utility>
+#include <algorithm>
+
+namespace lab {
+
+    /*
+     * Protected constructors
+     */
+
+    SimpleString::SimpleString(const size_t length)
+            : buffer_(new wchar_t[length]), capacity_(length), length_(length) {}
+
+    SimpleString::SimpleString(const size_t length, const size_t capacity) {
+        assert((length <= capacity));
+
+        buffer_ = new wchar_t[capacity_ = capacity];
+        length_ = length;
+    }
+
+    /*
+     * Public constructors
+     */
+
+    SimpleString::SimpleString() : SimpleString((size_t) 0) {}
+
+    SimpleString::SimpleString(const size_t length, const wchar_t symbol) : SimpleString(length) {
+        for (size_t i = 0; i < length; ++i) buffer_[i] = symbol;
+    }
+
+    SimpleString::SimpleString(char const *c_string) : SimpleString(strlen(c_string)) {
+        // note: trailing '\0' is not copied
+        mbstowcs(buffer_, c_string, length_);
+    }
+
+    /*
+     * Special constructors
+     */
+
+    SimpleString::SimpleString(const SimpleString &original) : SimpleString(original.length_) {
+        std::copy(original.buffer_, original.buffer_ + length_, buffer_);
+    }
+
+    SimpleString::SimpleString(SimpleString &&original) noexcept
+            : buffer_(original.buffer_), capacity_(original.capacity_), length_(original.length_) {
+        original.buffer_ = nullptr;
+    }
+
+    /*
+     * Public destructor
+     */
+
+    SimpleString::~SimpleString() {
+        delete[] buffer_;
+    }
+
+    /*
+     * Special operators
+     */
+
+    SimpleString &SimpleString::operator=(const SimpleString &original) {
+        if (this != &original) {
+            const auto length = original.length_;
+            if (length != length_) {
+                // a new buffer should be allocated
+
+                // free current buffer
+                delete[] buffer_;
+
+                // it is recommended to keep defaults in case something fails later
+                buffer_ = nullptr;
+                capacity_ = length_ = 0;
+
+                // create needed copies and assign them to the fields
+                buffer_ = new wchar_t[length];
+                capacity_ = length_ = length;
+            }
+
+            std::copy(original.buffer_, original.buffer_ + length, length);
+        }
+
+        return *this;
+    }
+
+    SimpleString &SimpleString::operator=(SimpleString &&original) noexcept {
+        if (this != &original) {
+            // free current buffer
+            delete[] buffer_;
+
+            buffer_ = std::exchange(original.buffer_, nullptr);
+            capacity_ = std::exchange(original.capacity_, 0);
+            length_ = std::exchange(original.length_, 0);
+        }
+
+        return *this;
+    }
+
+    SimpleString &SimpleString::operator=(SimpleString original) noexcept {
+        std::swap(buffer_, original.buffer_);
+        std::swap(capacity_, original.capacity_);
+        std::swap(length_, original.length_);
+
+        return *this;
+    }
+}
--- simple_string.h
+++ simple_string.h
@@ -2,9 +2,116 @@
 #define SEM_2_LAB_1_SIMPLE_STRING_H
 
 
-namespace lab {
-    class SimpleString {
+#include <cstddef>
+#include <cstring>
 
+namespace lab {
+
+    /**
+     * @brief Simple implementation of a
+     */
+    class SimpleString {
+    protected:
+
+        /**
+         * @brief Character buffer, its length is at least {@code length_}
+         *
+         * @note stored string is not 0-terminated
+         */
+        wchar_t *buffer_;
+
+        /**
+         * @brief Length of allocated {@code buffer_}
+         */
+        size_t capacity_,
+        /**
+         * @brief Length of the meaningful part of {@code buffer_}
+         */
+        length_;
+
+        /*
+         * Protected constructor
+         */
+
+        /**
+         * @brief Creates a new simple string of given length with no extra buffer space
+         *
+         * @param length length of the created string
+         */
+        explicit SimpleString(size_t length);
+
+        /**
+         * @brief Creates a new simple string of given length and capacity
+         *
+         * @param length length of the created string
+         * @param capacity capacity of the created string
+         * @throws if {@code capacity_} is less than {@code length_}
+         */
+        explicit SimpleString(size_t length, size_t capacity);
+
+    public:
+
+        /*
+         * Public constructors
+         */
+
+        /**
+         * @brief Creates a new empty string
+         */
+        SimpleString();
+
+        /**
+         * @brief Creates a new string of given size with its content set to {@ode #symbol}
+         *
+         * @param length length of the created string
+         * @param symbol symbol to fill the string with
+         */
+        SimpleString(size_t length, wchar_t symbol);
+
+        /**
+         * @brief Creates a new string based on the given C-string (0-terminated dynamic {@code char}-array)
+         *
+         * @param c_string original string to be copied into the created one
+         */
+        explicit SimpleString(char const *c_string);
+
+        /*
+         * Special constructors
+         */
+
+        /**
+         * @brief Copies the original string into the created one
+         *
+         * @param original string which should be copied into the created one
+         * @note the created string will have no extra buffer space
+         */
+        SimpleString(const SimpleString &original);
+
+        /**
+         * @brief Moves the original string into the created one
+         *
+         * @param original string which should be moved into the created one
+         */
+        SimpleString(SimpleString &&original) noexcept;
+
+        /*
+         * Public destructor
+         */
+
+        /**
+         * @brief Destroys this string freeing all dynamically allocated memory (i.e. {@code buffer_})
+         */
+        ~SimpleString();
+
+        /*
+         * Special operators
+         */
+
+        SimpleString &operator=(const SimpleString &original);
+
+        SimpleString &operator=(SimpleString &&original) noexcept;
+
+        SimpleString &operator=(SimpleString original) noexcept;
     };
 
     /*
