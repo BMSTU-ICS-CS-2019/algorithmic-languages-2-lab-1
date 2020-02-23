e9dce6cfc43d9b3772e0cad5b790c68fde15028a
PROgrammer_JARvis <gh@progrm-jarvis.ru>
2020-02-23 17:53:14 +0300

    Add class files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
+#include "simple_string.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
---
+++ simple_string.h
@@ -0,0 +1,16 @@
+#ifndef SEM_2_LAB_1_SIMPLE_STRING_H
+#define SEM_2_LAB_1_SIMPLE_STRING_H
+
+
+namespace lab {
+    class SimpleString {
+
+    };
+
+    /*
+     * Define `CustomString` as String
+     */
+    typedef SimpleString String;
+}
+
+#endif //SEM_2_LAB_1_SIMPLE_STRING_H
